1e71c0b17a604f0d6b0ab46512b9f7fb7962476e
nauqi-1 <nauqi2208@gmail.com>
2024-11-03 01:16:39 +0700

    partial LevelModel & TugasModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,26 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserModel extends Model
 {
     use HasFactory;
 
     protected $table    = 'm_user';
-    protected $primary  = 'user_id';
+    protected $primaryKey  = 'user_id';
     protected $fillable = ['level_id','username','password', 'created_at', 'updated_at'];
     protected $hidden   = ['password'];
 
     protected $casts    = ['password' => 'hashed'];
+
 
     public function level():BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    public function penjualan() 
+    public function tugas():HasMany
     {
-        return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
+        return $this->hasMany(TugasModel::class, 'tugas_pembuat_id', 'user_id');
     }
 
     //mendapatkan nama role
