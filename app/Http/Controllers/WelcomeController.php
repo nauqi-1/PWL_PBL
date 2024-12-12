3d48aa355f4f17dd431b3ce30fe4ec3d5ff8161e
Ratih Paramita <significantratih@gmail.com>
2024-12-12 07:51:25 +0700

    Tampilan Grafik di Dashboard Admin

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
@@ -14,9 +15,72 @@ class WelcomeController extends Controller
         ];
         $user = Auth::user();
         $activemenu = 'dashboard';
+
+        //menghitung tugas
+        $totalTugas = DB::table('t_tugas')->count();
+
+        //menghitung tugas per user
+        $totalTugasUser = DB::table('t_tugas')
+        ->where('tugas_pembuat_id', $user->user_id)
+        ->count();
+
+        //menghitung tugas per status
+        $totalTugasStatus = DB::table('t_tugas')
+        ->where('tugas_pembuat_id', $user->user_id)
+        ->select(DB::raw("tugas_status, COUNT(*) as count"))
+        ->groupBy('tugas_status')
+        ->pluck('count', 'tugas_status');
+
+        //menghitung tugas baru dibuat per bulan
+        $totalTugasBulan = DB::table('t_tugas')
+        ->select(DB::raw('MONTH(tugas_tgl_dibuat) as month'), DB::raw('COUNT(*) as count'))
+        ->where('tugas_pembuat_id', $user->user_id)
+        ->groupBy(DB::raw('MONTH(tugas_tgl_dibuat)'))
+        ->orderBy(DB::raw('MONTH(tugas_tgl_dibuat)'))
+        ->pluck('count', 'month');
+
+        //menghitung tugas per jenis
+        $totalTugasJenis = DB::table('t_tugas_jenis')
+        ->leftJoin('t_tugas', 't_tugas_jenis.jenis_id', '=', 't_tugas.jenis_id')
+        ->select('t_tugas_jenis.jenis_nama', DB::raw('COUNT(t_tugas.tugas_id) as total'))
+        ->groupBy('t_tugas_jenis.jenis_nama')
+        ->pluck('total', 't_tugas_jenis.jenis_nama');
+        //->toArray();
+
+        //menghitung request tugas
+        $totalRequest = DB::table('t_request')
+        ->where('status_request', 'P')
+        ->count();
+
+        //menghitung mhs alfa
+        $totalMhsAlfa = DB::table('t_mahasiswa_alfa')->count();
+
+        // Mengambil data periode
+        $periods = DB::table('m_periode')->pluck('periode', 'periode_id');
+
+        // Menghitung jumlah mahasiswa alfa dan kompen per periodik
+        $totalAlfaKompen = [];
+        foreach ($periods as $periode_id => $periode) {
+            $totalAlfa = DB::table('t_mahasiswa_alfa')
+                ->where('periode_id', $periode_id)
+                ->count();
+
+            $totalKompen = DB::table('t_mahasiswa_alfa')
+                ->join('m_mahasiswa', 't_mahasiswa_alfa.mahasiswa_id', '=', 'm_mahasiswa.mahasiswa_id')
+                ->where('t_mahasiswa_alfa.periode_id', $periode_id)
+                ->whereColumn('t_mahasiswa_alfa.jumlah_alfa', '=', 'm_mahasiswa.mahasiswa_alfa_lunas')
+                ->count();
+
+            $totalAlfaKompen[] = [
+                'periode' => $periode,
+                'alfa' => $totalAlfa,
+                'kompen' => $totalKompen,
+            ];
+        }
+
         switch($user->level_id) {
             case 1:
-                return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activemenu]);
+                return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activemenu, 'totalTugas' => $totalTugas, 'totalTugasUser' => $totalTugasUser, 'totalTugasStatus' =>$totalTugasStatus, 'totalTugasBulan' => $totalTugasBulan, 'totalTugasJenis' => $totalTugasJenis, 'totalRequest' => $totalRequest, 'totalMhsAlfa' => $totalMhsAlfa, 'totalAlfaKompen' => $totalAlfaKompen]);
             case 2:
                 return view('dosen.welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activemenu]);
             case 3:
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!--Datatables-->
   <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
   <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
